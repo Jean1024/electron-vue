6f3117f326d346f3e58ac9b7f4b4d0a30faa0b4a
zhilin <1737923789@qq.com>
2018-01-11 16:21:16 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'bpa'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
